0d32d68411506b7a50d39128095d36385d96ada5
redtime2002@naver.com <redtime2002@naver.com>
2017-08-25 18:38:30 +0900

    test

--- mailTest.py
+++ mailTest.py
@@ -83,7 +83,7 @@ def makeConversation(num):
     
     i = 0
     
-    while num > isdd:
+    while num > i:
        
         logOut()
         waitForIsElementPresent("//*[@id='container']/div/div[2]/div[1]/div/a/span")
